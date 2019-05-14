b0266260cc5acd7b6ba59a33c3179ea06d1f7b32
Curtis NJIKI DJAPA P <njikicurtis@gmail.com>
2019-05-14 14:48:05 +0100

    Détails d'une pharmacie

---
+++ app/src/main/java/com/example/curtisnjikidjapap/drugadvisor/DetailPharmacyActivity.java
@@ -0,0 +1,21 @@
+package com.example.curtisnjikidjapap.drugadvisor;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailPharmacyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_pharmacy);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
